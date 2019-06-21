29d0e0a85c618c1a9f273f518084413fe83b5bf2
Hugo Adan <hugo@vauxoo.com>
2019-06-21 00:44:32 +0000

    [MIG] crm_facebook_leads: Migration to 12.0

--- crm_facebook_leads/__manifest__.py
+++ crm_facebook_leads/__manifest__.py
@@ -8,14 +8,14 @@
     'description': """
     """,
 
-    'author': "BADEP",
-    'website': "https://badep.ma",
+    'author': "BADEP, Vauxoo",
+    'website': "https://badep.ma, www.vauxoo.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Lead Automation',
-    'version': '11.0.5',
+    'version': '12.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['crm'],
